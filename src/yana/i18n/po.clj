b7c8f749c6ca80fec5d1ee1452cd460b1b5dbd37
Yana Teras <yana@yana.moe>
2024-12-22 04:43:56 +0000

    Spec and implement contexts

--- src/yana/i18n/po.clj
+++ src/yana/i18n/po.clj
@@ -6,7 +6,15 @@
   (:refer-clojure :exclude [read-string])
   (:require [blancas.kern.core :as kern]
             [blancas.kern.lexer :as lex]
+            [clojure.spec.alpha :as s]
             [clojure.string :as str]))
+
+(s/def ::ctxt (s/nilable string?))
+(s/def ::id string?)
+(s/def ::str string?)
+
+(s/def ::map
+  (s/map-of ::ctxt (s/map-of ::id ::str)))
 
 (def ^:private po-style
   (assoc lex/basic-def :comment-line "#"))
@@ -17,14 +25,34 @@
 (def ^:private po-parser
   (let [{:keys [string-lit token trim]} po]
     (kern/bind [_ trim
-                _ (token "msgid")
-                k string-lit
-                _ (token "msgstr")
-                v (kern/many string-lit)]
-      (kern/return [k (str/join v)]))))
+                c (kern/optional
+                   (kern/>>
+                    (token "msgctxt")
+                    (kern/many string-lit)))
+                i (kern/>>
+                   (token "msgid")
+                   (kern/many string-lit))
+                s (kern/>>
+                   (token "msgstr")
+                   (kern/many string-lit))]
+               (kern/return (map #(if % (str/join %)) [c i s])))))
 
 (defn read-string [s]
-  {nil (into {} (kern/value (kern/many po-parser) s))})
+  (reduce (fn [m [c i s]]
+            (assoc-in m [c i] s))
+          {}
+          (kern/value (kern/many po-parser) s)))
+
+(s/fdef read-string
+  :args (s/cat :s string?)
+  :ret ::map)
+
+(def ^:private po-format-with-ctxt
+  "msgctxt \"%s\"
+msgid \"%s\"
+msgstr \"%s\"
+
+")
 
 (def ^:private po-format
   "msgid \"%s\"
@@ -33,4 +61,13 @@ msgstr \"%s\"
 ")
 
 (defn write-string [m]
-  (str/join (map (partial apply format po-format) (m nil))))
+  (str/join
+     (for [[c is] m
+           [i s] is]
+       (if c
+         (format po-format-with-ctxt c i s)
+         (format po-format i s)))))
+
+(s/fdef write-string
+  :args (s/cat :m ::map)
+  :ret string?)
